93687615db9de5781111415ab2c79c9c72c45127
vitosotdihaet <vitostikitos@gmail.com>
2025-12-25 11:37:41 +0300

    feat: add logs for scraping ETA

--- scraping/src/scrapers/utils.py
+++ scraping/src/scrapers/utils.py
@@ -2,8 +2,14 @@ import requests
 from exception import ParserException
 
 
+proxies = {
+    "http": "socks5h://127.0.0.1:1080",
+    "https": "socks5h://127.0.0.1:1080",
+}
+
+
 def get_from_url(url: str) -> str:
-    response = requests.get(url)
+    response = requests.get(url, proxies=proxies)
     response.raise_for_status()
     if response.text is None:
         raise ParserException("Response text is None")
@@ -11,6 +17,6 @@ def get_from_url(url: str) -> str:
 
 
 def get_response(url: str) -> requests.Response:
-    response = requests.get(url)
+    response = requests.get(url, proxies=proxies)
     response.raise_for_status()
     return response
